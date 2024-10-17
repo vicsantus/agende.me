2403333358ec9d592a5fc3f1a86a09862cf70d95
victor santos <victor.santos@lexartlabs.xyz>
2024-10-17 13:29:26 -0300

    fix: Change vue to react native and creating a api with db mysql

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM node:18-alpine
-WORKDIR /app
-COPY . /app
-RUN npm install
-EXPOSE 8080
-CMD ["npm", "run", "serve"]
--- src/formkit.config.js
+++
@@ -1,8 +0,0 @@
-import { pt } from "@formkit/i18n";
-
-const config = {
-  locales: { pt },
-  locale: "pt",
-};
-
-export default config;
